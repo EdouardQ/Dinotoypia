0c2e081c4d3a6214d9f3fd5d52ad140cc9396a30
EdouardQ <edouard.quilliou@gmail.com>
2022-01-03 16:32:41 +0100

    commit 0.15
    ajout method CRUD de product
    fix name des form

--- src/Controller/UserBack/ProductController.php
+++ src/Controller/UserBack/ProductController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller\UserBack;
 
+use App\Entity\Product;
+use App\Form\ProductFormType;
 use App\Repository\ProductRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use http\Env\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,10 +15,12 @@ use Symfony\Component\Routing\Annotation\Route;
 class ProductController extends AbstractController
 {
     private ProductRepository $productRepository;
+    private EntityManagerInterface $entityManager;
 
-    public function __construct(ProductRepository $productRepository)
+    public function __construct(ProductRepository $productRepository, EntityManagerInterface $entityManager)
     {
         $this->productRepository = $productRepository;
+        $this->entityManager = $entityManager;
     }
 
     #[Route('/', name: 'user_back.product.index')]
@@ -22,6 +28,42 @@ class ProductController extends AbstractController
     {
         return $this->render('user_back/product/index.html.twig', [
             'listProduct' => $this->productRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/create', name: 'user_back.product.create')]
+    public function create(Request $request): Response
+    {
+        $entity = new Product();
+        $form = $this->createForm(ProductFormType::class, $entity);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->entityManager->persist($entity);
+            $this->entityManager->flush($entity);
+
+            return $this->redirectToRoute('user_back.product.index');
+        }
+
+        return $this->render('user_back/product/create.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/update/{id}', name: 'user_back.product.update')]
+    public function update(Product $entity, Request $request): Response
+    {
+        $form = $this->createForm(ProductFormType::class, $entity);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->entityManager->flush($entity);
+
+            return $this->redirectToRoute('user_back.product.index');
+        }
+
+        return $this->render('user_back/product/update.html.twig', [
+            'form' => $form->createView(),
         ]);
     }
 }
--- src/Form/ProductCategoryFormType.php
+++ src/Form/ProductCategoryFormType.php
@@ -9,7 +9,7 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\NotBlank;
 
-class ProductCategoryType extends AbstractType
+class ProductCategoryFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
