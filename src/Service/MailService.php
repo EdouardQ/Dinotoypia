6b0b7b76f41506b11a8db929474cbfafa590643f
EdouardQ <edouard.quilliou@gmail.com>
2022-06-12 19:00:39 +0200

    commit 0.46
    setup mailservice

--- src/Service/MailService.php
+++ src/Service/MailService.php
@@ -3,15 +3,19 @@
 namespace App\Service;
 
 use Symfony\Component\HttpKernel\KernelInterface;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 
 class MailService
 {
     private $env;
+    private MailerInterface $mailer;
     private bool $prod;
 
-    public function __construct(KernelInterface $kernel)
+    public function __construct(KernelInterface $kernel, MailerInterface $mailer)
     {
         $this->env = $kernel->getEnvironment();
+        $this->mailer = $mailer;
         $this->prod = $this->env === "dev";
     }
 
