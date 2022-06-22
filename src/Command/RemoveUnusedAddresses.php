4b25530c5f5198cd951bc06d39459133a18f4b24
EdouardQ <edouard.quilliou@gmail.com>
2022-06-22 15:51:46 +0200

    Update RemoveUnusedAddresses.php

--- src/Command/RemoveUnusedAddresses.php
+++ src/Command/RemoveUnusedAddresses.php
@@ -4,13 +4,10 @@ namespace App\Command;
 
 use App\Entity\BillingAddress;
 use App\Entity\DeliveryAddress;
-use App\Repository\OrderRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 
