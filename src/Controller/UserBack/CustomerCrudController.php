f30ada238b7ad4e39b566b32d3e7dfdcc796c0c9
Jogroe <56687596+Jogroe@users.noreply.github.com>
2022-06-22 15:35:48 +0200

    Commit 0.56

--- src/Controller/UserBack/CustomerCrudController.php
+++ src/Controller/UserBack/CustomerCrudController.php
@@ -43,10 +43,6 @@ class CustomerCrudController extends AbstractCrudController
             EmailField::new('email'),
             TextField::new('firstName')->setLabel('Prénom'),
             TextField::new('lastName')->setLabel('Nom'),
-            TextField::new('address')->setLabel('Adresse'),
-            TextField::new('city')->setLabel('Ville'),
-            TextField::new('postCode')->setLabel('Code postal'),
-            CountryField::new('country')->setLabel('Pays'),
             TelephoneField::new('phone')->setLabel('Téléphone'),
         ];
     }
