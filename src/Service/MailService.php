fb314c2e0bcf9984af45ac5de9df9a42948563f4
Jogroe <56687596+Jogroe@users.noreply.github.com>
2022-06-26 16:23:39 +0200

    Revert "Commit 0.67"
    This reverts commit 256fb7802b85095a497c1302e562fbd6217a6531.

--- src/Service/MailService.php
+++ src/Service/MailService.php
@@ -2,10 +2,9 @@
 
 namespace App\Service;
 
-use App\Entity\Order;
-use Symfony\Bridge\Twig\Mime\TemplatedEmail;
-use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\HttpKernel\KernelInterface;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 
 class MailService
 {
@@ -26,22 +25,5 @@ class MailService
             dd('ok');
         }
         dd('pas ok');
-    }
-
-    public function sendEmailOrder(Order $order): void
-    {
-        if($this->prod) {
-            $email = (new TemplatedEmail())
-                ->from('no-reply@dinotoypia.store')
-                ->to($order->getCustomer()->getEmail())
-                ->subject('Confirmation de commande')
-                ->htmlTemplate('emails/order.html.twig')
-                ->context([
-                    'order' => $order,
-                    'totalPrice' => $order->getTotalPriceOfOrderItems()
-                ]);
-
-            $this->mailer->send($email);
-        }
     }
 }
