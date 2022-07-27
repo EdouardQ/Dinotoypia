18d129816d8b5c894b5979cc0615980bc49cb7ef
devFendrix <dev.fendrix@gmail.com>
2022-07-27 22:56:19 +0200

    commit 0.118
    add cat + product page

--- src/Controller/ProductCategoryController.php
+++ src/Controller/ProductCategoryController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\ProductCategory;
 use App\Repository\ProductRepository;
+use App\Repository\ProductCategoryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,12 +12,21 @@ use Symfony\Component\Routing\Annotation\Route;
 #[Route('/category')]
 class ProductCategoryController extends AbstractController
 {
-    #[Route('/{id}',  name: 'category.index')]
-    public function index(ProductCategory $productCategory, ProductRepository $productRepository): Response
+    #[Route('/',  name: 'category.index')]
+    public function index(ProductCategoryRepository $productCategoryRepository): Response
+    {
+        return $this->render('category/index.html.twig', [
+            'categoryList' => $productCategoryRepository->findAll()
+        ]);
+    }
+
+    #[Route('/{id}',  name: 'category.cat')]
+    public function productCategory(ProductCategory $productCategory, ProductRepository $productRepository): Response
     {
         $productList = $productRepository->findByCatogory($productCategory);
-        return $this->render('category/index.html.twig', [
-            'productList' => $productList
+        return $this->render('category/category_products.html.twig', [
+            'productList' => $productList,
+            'nameCategory' => $productCategory->getName()
         ]);
     }
 }
